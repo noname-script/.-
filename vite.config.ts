434438ab391663fbb60f4b762e8fcfaec2e8e3a8
noname <ioscarl0129@gmail.com>
2025-11-25 01:05:45 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(process.cwd(), './'),
+      },
+    },
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
+    },
+    build: {
+      outDir: 'dist',
+    },
+  };
+});
